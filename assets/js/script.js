0d20de6e98b264d4f44fd820a53a68bb672aa63c
Rafael Brito Velasquez <recuperarrafael.v@gmail.com>
2020-08-20 23:52:44 -0400

    tooltips y otros detalles

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+})
+$(function(){
+
+    $('a').click(function(event) {
+        if (this.hash !== "") {
+          event.preventDefault();
+  
+          var gato = this.hash; //guarda el valor de hash en una variable llamada gato. actualmete en vez de var se denomina let o cons
+  
+          $("html, body").animate({
+            scrollTop: $(gato).offset().top
+          }, 600, function(){  //600 milisegundos de retardo para abrir
+  
+            window.location.hash = gato; //Agrega hash (#) a la URL cuando haya terminado de desplazarse. comportamiento de click predeterminado
+          });
+        } //fin del if
+    });
+  
+    $('[data-toggle="popover"]').popover();
+  })
